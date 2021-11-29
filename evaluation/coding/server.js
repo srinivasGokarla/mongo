fc197d870029b5f5f341f323771b84c4e05fdc4a
srinivas217 <87422030+srinu217@users.noreply.github.com>
2021-11-29 15:27:51 +0530

    4th

--- evaluation/coding/server.js
+++ evaluation/coding/server.js
@@ -1,6 +1,17 @@
  const express = require("express")
  const mongoose = express("mongoose")
 
+ const jobs = require("./jobs.json")
+
+
+ const connect = () => {
+     return mongoose.connect("mongodb://localhost:27017/test", {
+         useNewUrlParser : true,
+         useCreateIndex : true,
+         useUnifiedTopology: true,
+     })
+ }
+
  const app = express()
 
 app.listen(3300, function() {
