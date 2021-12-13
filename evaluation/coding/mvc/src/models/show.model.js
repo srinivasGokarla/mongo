5c58297ca661eed0799c232c5ff212d4794ee0ff
srinivas217 <87422030+srinu217@users.noreply.github.com>
2021-12-13 16:50:57 +0530

    8th

--- evaluation/coding/mvc/src/models/show.model.js
+++ evaluation/coding/mvc/src/models/show.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const showSchema = new mongoose.Schema({
     timing : {type: Number, required: true},
-    movie : [{type: mongoose.Schema.Types.ObjectId, ref:"theatre", required: true}],
+    movie : [{type: mongoose.Schema.Types.ObjectId, ref:"movie", required: true}],
     screen : [{type: mongoose.Schema.Types.ObjectId, ref:"screen", required: true}],
     seats:  {type: Number, required: true}
     
