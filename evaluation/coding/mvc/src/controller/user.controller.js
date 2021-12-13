3838c09dd7891ded62457b215a5da1612b945225
srinivas217 <87422030+srinu217@users.noreply.github.com>
2021-12-13 15:09:34 +0530

    2nd

--- evaluation/coding/mvc/src/controller/user.controller.js
+++ evaluation/coding/mvc/src/controller/user.controller.js
@@ -3,4 +3,19 @@ const express = require("express")
 const router = express.Router();
 
 const User = require("../models/user.model")
+const crudController = require("./crud.controller")
 
+router.post("",crudController(User))
+router.get("", async(req,res) => {
+    const users = await User.find().sort({id:-1}).lean().exec()
+
+    return res.status(200).send({users})
+});
+
+
+router.patch("/:id",crudController.updateOne(User))
+router.delete("/:id",crudController.deleteOne(User))
+router.get("/:id",crudController.geteOne(User))
+
+
+module.exports = router
